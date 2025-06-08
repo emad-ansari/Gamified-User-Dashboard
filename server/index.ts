f0ae7c450df6e5c0ae287ad893f0e789a20db1e9
92Mohammad <m.e89571828@gmail.com>
2025-06-08 23:07:41 +0530

    added user and auth route

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,10 @@ require('dotenv').config()
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./src/config/db";
+import authRoutes from "./src/routes/authRoutes";
+import userRoutes from "./src/routes/userRoutes";
+
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +14,11 @@ const MONGO_URI = process.env.MONGODB_URL as string;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
 
 app.get("/", (req, res) => {
 	res.send("API is running...");
