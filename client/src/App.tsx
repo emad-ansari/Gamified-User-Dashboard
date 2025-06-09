9b2bdbfdcd82974b04471dacb7b2e5685f3d6469
92Mohammad <m.e89571828@gmail.com>
2025-06-09 21:43:16 +0530

    added routes for setting and statistic page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
 import Dashboard from '@/pages/Dashboard';
+import { Statistics } from '@/pages/Statistics';
+import { Settings } from '@/pages/Settings';
 
 function App() {
 	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<LoginPage />} />
-          <Route path = "/dashboard" element={<Dashboard />} />
-				</Routes>
-			</BrowserRouter>
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/statistics" element={<Statistics />} />
+				<Route path="/settings" element={<Settings />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</Router>
 	);
 }
 
