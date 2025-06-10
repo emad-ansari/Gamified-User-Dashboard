ff3bad276765ee5998ef5f32f7b02a0dd427b3d3
92Mohammad <m.e89571828@gmail.com>
2025-06-10 09:11:59 +0530

    added mood, habit , journal route

--- server/index.ts
+++ server/index.ts
@@ -4,13 +4,12 @@ import cors from "cors";
 import { connectDB } from "./src/config/db";
 import authRoutes from "./src/routes/authRoutes";
 import userRoutes from "./src/routes/userRoutes";
-
+import journalRoutes from "./src/routes/journalRoutes";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const MONGO_URI = process.env.MONGODB_URL as string;
-
 
 app.use(cors());
 app.use(express.json());
@@ -18,15 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-
-
-app.get("/", (req, res) => {
-	res.send("API is running...");
-});
-
+app.use('/api/journal', journalRoutes);
 
 connectDB(MONGO_URI).then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server is running at http://localhost:${PORT}`);
   });
+}).catch(err => {
+  console.error('Failed to connect to MongoDB:', err);
 });
