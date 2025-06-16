abfbd9c71eaa587388090944e708df91478fd0fe
92Mohammad <m.e89571828@gmail.com>
2025-06-16 16:03:49 +0530

    added build config to specify the output directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: { manifest: true, outDir: './build'},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
